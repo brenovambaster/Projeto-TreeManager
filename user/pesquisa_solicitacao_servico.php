49d5a573aaa7b552b4236fcf15a62fb71a1ef48a
Breno Vambaster <brenovambaster5@gmail.com>
2020-04-28 22:17:25 -0300

    melhotamento da estética do codigo

--- user/pesquisa_solicitacao_servico.php
+++ user/pesquisa_solicitacao_servico.php
@@ -1,52 +1,115 @@
+<?php
+require_once('../00 - BD/bd_conexao.php');
 
-        <?php
-        require_once('../00 - BD/bd_conexao.php');
+
+function determinaPesquisaSql()
+{
+        $idUsu = $_SESSION['idUsu'];
+        if (!isset($_GET['pesquisa'])) {
+                $filtro = "";
+                $sql = " SELECT * from servico    inner join tiposervico  on(tipoServico = IdTipoServico) where usuSolicitante = '$idUsu' ORDER BY statusSer asc "; // SELECIONA OS USUARIOS POR ORDEM ALFABÉTICA CRESCENTE
+
+        }
+        if (isset($_GET['pesquisa'])) {
+
+                $data = $_GET['data'];
+                $codArvore = $_GET['codArvore'];
 
 
-        function determinaPesquisaSql()
-        {
-                $idUsu = $_SESSION['idUsu'];
-                if (!isset($_GET['pesquisa'])) {
+                if (!empty($data) && empty($codArvore)) {
+
+                        // pesquisar por data
+                        $filtro = "Filtro por data";
+                        $sql = "SELECT * from servico inner join tiposervico on (tipoServico = IdTipoServico) where usuSolicitante = '$idUsu' AND  dataServico ='$data'";
+                } else if (!empty($codArvore) && empty($data)) {
+                        // pesquisar por codArvore
+                        $sql = "SELECT * FROM servico inner join tiposervico on (tipoServico=IdTipoServico) where usuSolicitante = '$idUsu' AND codArvore = '$codArvore'";
+                        $filtro = "Filtro por CodArvore";
+                }
+                if (!empty($data) && !empty($codArvore)) {
+                        // pesquisar pelos dois campos
+                        $filtro = "Filtro por data e codArvore";
+                        $sql = "SELECT * FROM servico inner join tiposervico on (tipoServico=IdTipoServico) where usuSolicitante = '$idUsu'AND codArvore ='$codArvore' AND dataServico='$data'";
+                }
+                if (empty($data) && empty($codArvore)) {
+                        // pesquisa sem filtro;
+
                         $filtro = "";
                         $sql = " SELECT * from servico    inner join tiposervico  on(tipoServico = IdTipoServico) where usuSolicitante = '$idUsu' ORDER BY statusSer asc "; // SELECIONA OS USUARIOS POR ORDEM ALFABÉTICA CRESCENTE
 
                 }
-                if (isset($_GET['pesquisa'])) {
-
-                        $data = $_GET['data'];
-                        $codArvore = $_GET['codArvore'];
-
-
-                        if (!empty($data) && empty($codArvore)) {
-
-                                // pesquisar por data
-                                $filtro = "Filtro por data";
-                                $sql = "SELECT * from servico inner join tiposervico on (tipoServico = IdTipoServico) where usuSolicitante = '$idUsu' AND  dataServico ='$data'";
-                        } else if (!empty($codArvore) && empty($data)) {
-                                // pesquisar por codArvore
-                                $sql = "SELECT * FROM servico inner join tiposervico on (tipoServico=IdTipoServico) where usuSolicitante = '$idUsu' AND codArvore = '$codArvore'";
-                                $filtro = "Filtro por CodArvore";
-                        }
-                        if (!empty($data) && !empty($codArvore)) {
-                                // pesquisar pelos dois campos
-                                $filtro = "Filtro por data e codArvore";
-                                $sql = "SELECT * FROM servico inner join tiposervico on (tipoServico=IdTipoServico) where usuSolicitante = '$idUsu'AND codArvore ='$codArvore' AND dataServico='$data'";
-                        }
-                        if (empty($data) && empty($codArvore)) {
-                                // pesquisa sem filtro;
-
-                                $filtro = "";
-                                $sql = " SELECT * from servico    inner join tiposervico  on(tipoServico = IdTipoServico) where usuSolicitante = '$idUsu' ORDER BY statusSer asc "; // SELECIONA OS USUARIOS POR ORDEM ALFABÉTICA CRESCENTE
-
-                        }
-                }
-                return  $sql = array(
-                        'sql' => $sql,
-                        'filtro' => $filtro
-                );
         }
-        $sql = determinaPesquisaSql();
-        $resultado = $con->query($sql['sql']);
-        fecharConexao($con);
-        ?>
+        return  $sql = array(
+                'sql' => $sql,
+                'filtro' => $filtro
+        );
+}
 
+$sql = determinaPesquisaSql();
+$resultado = $con->query($sql['sql']);
+fecharConexao($con);
+if (mysqli_num_rows($resultado) == 0) {
+        echo "<h5>Sua pesquisa não gerou nenhum resultado. Ou você ainda não possui nenhum registro de 
+                solicitação de serviço.</h5>";
+} else {
+        verTabela($resultado);
+}
+
+
+function verTabela($resultado)
+{
+?>
+        <table class="table table-striped  ">
+                <thead>
+                        <tr class="bg-success ">
+                                <th scope="col">ID</th>
+                                <th scope="col">CodAvore</th>
+                                <th scope="col">Data</th>
+                                <th scope="col">Tipo Servico</th>
+                                <th scope="col">Status</th>
+                                <th>Descrição</th>
+                        </tr>
+                </thead>
+                <tbody>
+
+                        <?php while ($infoSol = mysqli_fetch_object($resultado)) { ?>
+                                <tr>
+                                        <th> <?php echo $infoSol->codServico; ?> </th>
+                                        <th> <?php echo $infoSol->codArvore; ?> </th>
+                                        <td>
+                                                <?php
+                                                $date = DateTime::createFromFormat('Y-m-d', $infoSol->dataServico);
+                                                echo $date->format('d/m/Y');
+                                                ?>
+                                        </td>
+
+                                        <td><?php echo $infoSol->NomeServico; ?></td>
+                                        <th>
+
+
+                                                <?php echo $infoSol->statusSer;  ?>
+
+                                        </th>
+                                        <td>
+                                                <a class="btn-link" href="detalhes_solicitacao.php?id=<?php echo $infoSol->codServico; ?>">
+                                                        <span class="d-inline-block text-truncate" style="max-width: 150px;">
+                                                                <?php echo $infoSol->descricao; ?>
+                                                        </span>
+                                                </a>
+                                        </td>
+                                        </div>
+
+
+                                        </td>
+                                </tr>
+                        <?php } // fechar while 
+                        ?>
+                </tbody>
+
+        </table>
+
+<?php
+
+}
+
+?>
--- user/solicitacoes.php
+++ user/solicitacoes.php
@@ -36,16 +36,6 @@ include('seguranca.php');
 
     </div>
 
-    <!--  ####################   IMPORTANTE!!!  #####################
-     PRECISO COLOCAR A FUNÇÃO 'LIDA' PARA SER MOSTRADA NA TABELA DE NOTIFICAÇÕES. 
-        ~Como Funciona?~ 
-            Quando o usuário clicar em "ver" será redirecionado para a pagina 'verSolicitacao.php'.Nessa página, o usuário vai ver 
-            os dados da solicitação e poderá marcar a notificação como 'lida' ou nao. Se maracar a solicitção como lida, irá mostrar 
-            na tabela de solicitações a opção marcada. Por enquanto, visualmete será desse modo.No entanto não está implemtentado de forma 
-            correta, pois há inconsistência.   
-        
-    -->
-
     <div class="container row mx-auto mb-4">
         <div class="pesquisa-form form-group bloco-form col-md-3 mt-2  border-right ">
             <form action="solicitacoes.php" method="GET">
@@ -67,96 +57,21 @@ include('seguranca.php');
         </div>
         <?php
 
-        include("pesquisa_solicitacao_servico.php");
-
 
         ?>
 
+
         <div class=" col-md-9 mt-3 table table-responsive">
-            <table class="table table-striped  ">
-                <thead>
-                    <tr class="bg-success ">
-                        <th scope="col">ID</th>
-                        <th scope="col">CodAvore</th>
-                        <th scope="col">Data</th>
-                        <th scope="col">Tipo Servico</th>
-                        <th scope="col">Status</th>
-                        <th>Descrição</th>
-                    </tr>
-                </thead>
-                <tbody>
 
-                    <?php while ($infoSol = mysqli_fetch_object($resultado)) { ?>
-                        <tr>
-                            <th> <?php echo $infoSol->codServico; ?> </th>
-                            <th> <?php echo $infoSol->codArvore; ?> </th>
-                            <td>
-                                <?php
-                                $date = DateTime::createFromFormat('Y-m-d', $infoSol->dataServico);
-                                echo $date->format('d/m/Y');
-                                ?>
-                            </td>
+            <?php
+            include("pesquisa_solicitacao_servico.php");
 
-                            <td><?php echo $infoSol->NomeServico; ?></td>
-                            <th>
-
-
-                                <?php echo $infoSol->statusSer;  ?>
-
-                            </th>
-                            <td>
-                                <a class="btn-link" href="detalhes_solicitacao.php?id=<?php echo $infoSol->codServico; ?>">
-                                    <span class="d-inline-block text-truncate" style="max-width: 150px;">
-                                        <?php echo $infoSol->descricao; ?>
-                                    </span>
-                                </a>
-                            </td>
+            echo '<div class="badge badge-primary text-wrap">' . $sql['filtro'] . '</div>';
+            ?>
         </div>
 
 
-        </td>
-        </tr>
-    <?php } // fechar while 
-    ?>
-    </tbody>
-
-    </table>
-    <?php
-
-    echo '<div class="badge badge-primary text-wrap">' . $sql['filtro'] . '</div>';
-
-    ?>
-    <?php
-    if (mysqli_num_rows($resultado) == 0) {
-        echo "<h5>Sua pesquisa não gerou nenhum resultado. Ou você ainda não possui nenhum registro de 
-                solicitação de serviço.</h5>";
-    }
-    ?>
-
     </div>
-
-
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
