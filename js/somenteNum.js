81a2e288b56a83683705be3d48ab2828eff8539e
Breno Vambaster <brenovambaster5@gmail.com>
2020-05-01 11:31:05 -0300

    funcao para entrar com apenas nuemros no input

---
+++ js/somenteNum.js
@@ -0,0 +1,12 @@
+function somenteNumeros(e) {
+    var charCode = e.charCode ? e.charCode : e.keyCode;
+    // charCode 8 = backspace   
+    // charCode 9 = tab
+    if (charCode != 8 && charCode != 9) {
+        // charCode 48 equivale a 0   
+        // charCode 57 equivale a 9
+        if ((charCode < 48 && charCode != 46) || charCode > 57) {
+            return false;
+        }
+    }
+}
