009fec36f689750e6db1ee3ac402a0f6ca788e29
Breno Vambaster <brenovambaster5@gmail.com>
2020-05-28 20:52:31 -0300

    remover/inserir foto de perfil

---
+++ user/editar_foto.php
@@ -0,0 +1,74 @@
+<?php
+include('seguranca.php');
+require_once('../00 - BD/bd_conexao.php');
+$id = $_SESSION['idUsu'];
+$arquivo    = $_FILES['arquivo']['name'];
+$_UP['tamanho'] = 1024 * 1024 * 100; //5mb
+$formatoPermitido = array("png", "jpeg", "jpg", "gif");
+$extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+//Array com os tipos de erros de upload do PHP
+$_UP['erros'][0] = 'Não houve erro';
+$_UP['erros'][1] = 'O arquivo no upload é maior que o limite do PHP';
+$_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especificado no HTML';
+$_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
+$_UP['erros'][4] = 'Não foi feito o upload do arquivo';
+
+if ($_FILES['arquivo']['error'] != 0) {
+    echo ("Não foi possivel fazer o upload, erro: <br />" . $_UP['erros'][$_FILES['arquivo']['error']]);
+    echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=perfil.php'>
+   <script type=\"text/javascript\">
+      alert(\"Não foi possivel fazer o upload\");
+   </script>";
+
+
+
+    exit; //Para a execução do script
+}
+
+if ($_UP['tamanho'] < $_FILES['arquivo']['size']) {
+    echo "Arquivo Muito Grande. Máximo é 5MB";
+    exit;
+}
+
+if (in_array($extensao, $formatoPermitido)) {
+    $pasta = "foto_perfil/";
+    $temporario = $_FILES['arquivo']['tmp_name'];
+    $novoName = uniqid() . ".$extensao";
+
+    if (move_uploaded_file($temporario, $pasta . $novoName)) {
+        $mensagem = "Enviado com sucesso";
+    } else {
+        $mensagem = "falha ao enviar foto";
+    }
+} else {
+    echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=perfil.php'>
+   <script type=\"text/javascript\">
+      alert(\"A imagem não foi cadastrada. Extesão inválida.\");
+   </script>
+";
+    exit;
+}
+echo $mensagem;
+$sql = "UPDATE usuario SET  foto='$novoName' Where idUsuario='$id'";
+$resultado = $con->query($sql);
+if ($con->query($sql) === TRUE) {
+    echo "success editar";
+    echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=perfil.php'>
+    <script type=\"text/javascript\">
+        alert(\"A imagem  foi cadastrada com sucesso.\");
+    </script>
+";
+} else {
+    echo "erro editar";
+    echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=perfil.php'>
+    <script type=\"text/javascript\">
+        alert(\"A imagem não foi cadastrada \");
+    </script>
+";
+}
+
+fecharConexao($con);
