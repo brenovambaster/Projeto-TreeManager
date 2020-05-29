8d990ae8d8c82837639b00d31fcb4957b66b36d3
Breno Vambaster <brenovambaster5@gmail.com>
2020-05-29 18:46:57 -0300

    redirecionamento de pg e restricao de acessar a pag sem dados obrigatorios

--- user/solicitar_servico.php
+++ user/solicitar_servico.php
@@ -1,97 +1,104 @@
-<?php include("seguranca.php") ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php include("seguranca.php");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#343a40">
-    <title>Solicitar serviços</title>
-    <meta name="viewport" content="width=dsevice-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/solicitacao_servico.css">
-</head>
+if (!isset($_GET['id'])) { // querer entrar sem passar o id
+    echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=gerenciamento_arvores.php'>";
+    exit;
+} else {
+?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#343a40">
+        <title>Solicitar serviços</title>
+        <meta name="viewport" content="width=dsevice-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/solicitacao_servico.css">
+    </head>
 
-    <div class="servicos">
-        <!-- div top-total. DIV para todo o topo do site
+    <body>
+
+        <div class="servicos">
+            <!-- div top-total. DIV para todo o topo do site
             -->
-        <?php include('statusSession.php'); ?>
-        <div class="">
-            <h1> Solicitação de serviços </h1>
+            <?php include('statusSession.php'); ?>
+            <div class="">
+                <h1> Solicitação de serviços </h1>
 
+            </div>
+            <?php include('navbar.php'); ?>
         </div>
-        <?php include('navbar.php'); ?>
-    </div>
 
 
-    <section>
-        <div class="container">
-            <div>
-                <div class="offset-md-3 col-md-10">
-                    <form class="col-md-6 col-sm-10" method="POST" action="valida_servico.php">
-                        <div class="form-row">
-                            <div class="form-group col-md-3">
-                                <label for="id">ID Arvore:</label>
-                                <input class="form-control" name="idAvoreInp" id="id" type="text" value="  <?php echo $id = $_GET["id"];  ?> " readonly>
-                            </div>
+        <section>
+            <div class="container">
+                <div>
+                    <div class="offset-md-3 col-md-10">
+                        <form class="col-md-6 col-sm-10" method="POST" action="valida_servico.php">
+                            <div class="form-row">
+                                <div class="form-group col-md-3">
+                                    <label for="id">ID Arvore:</label>
+                                    <input class="form-control" name="idAvoreInp" id="id" type="text" value="  <?php echo $id = $_GET["id"];  ?> " readonly>
+                                </div>
 
-                            <div class="form-group col-md-7">
-
-                                <?php
-                                require_once("../00 - BD/bd_conexao.php");
-                                $sql = "SELECT * From tiposervico";
-                                $result = $con->query($sql);
-                                ?>
-
-                                <label for="selec"> Escolha o tipo de serviço</label>
-                                <select id="selec" name="servico" class=" custom-select form-control">
-                                    <?php
-                                    while ($infoTipServ = mysqli_fetch_object($result)) { ?>
-                                        <option value=" <?php echo $infoTipServ->idTipoServico; ?>"> <?php echo $infoTipServ->NomeServico; ?> </option>
+                                <div class="form-group col-md-7">
 
                                     <?php
-                                    }
-                                    fecharConexao($con);
+                                    require_once("../00 - BD/bd_conexao.php");
+                                    $sql = "SELECT * From tiposervico";
+                                    $result = $con->query($sql);
                                     ?>
 
+                                    <label for="selec"> Escolha o tipo de serviço</label>
+                                    <select id="selec" name="servico" class=" custom-select form-control">
+                                        <?php
+                                        while ($infoTipServ = mysqli_fetch_object($result)) { ?>
+                                            <option value=" <?php echo $infoTipServ->idTipoServico; ?>"> <?php echo $infoTipServ->NomeServico; ?> </option>
+
+                                        <?php
+                                        }
+                                        fecharConexao($con);
+                                        ?>
 
 
 
-                                </select>
+
+                                    </select>
+                                </div>
+
                             </div>
 
-                        </div>
 
-
-                        <div class="form-group">
-                            <label for="descricao"> Descreva o serviço</label>
-                            <textarea class="form-control" required name="descricaoServico" id="exampleFormControlTextarea1" rows="4"></textarea>
-                        </div>
+                            <div class="form-group">
+                                <label for="descricao"> Descreva o serviço</label>
+                                <textarea class="form-control" required name="descricaoServico" id="exampleFormControlTextarea1" rows="4"></textarea>
+                            </div>
 
 
 
 
-                        <button type="submit" class="btn btn-primary mb-4">Enviar</button>
-                    </form>
+                            <button type="submit" class="btn btn-primary mb-4">Enviar</button>
+                        </form>
+                    </div>
+
+
                 </div>
 
 
             </div>
 
+        </section>
 
-        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    </section>
+    </body>
 
+    </html>
 
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php
+}
+?>
--- user/valida_servico.php
+++ user/valida_servico.php
@@ -1,36 +1,60 @@
 <?php
 include("seguranca.php");
-$idUsu = $_SESSION["idUsu"];
-$idArvore = $_POST["idAvoreInp"];
-$tipoServico = $_POST["servico"];
-$descricaoServico = addslashes($_POST["descricaoServico"]);
-date_default_timezone_set("Brazil/East");
-$dataServico = date('yy,m,d');
-$status = "pendente";
+if ($_POST["idAvoreInp"] > 0) { // caso o id  seja  válido ele entra aqui. 
 
 
-echo "id arvore" . $idArvore;
-echo $dataServico;
+    $idUsu = $_SESSION["idUsu"];
+    $idArvore = '';
+    $idArvore = $_POST["idAvoreInp"];
+    $tipoServico = $_POST["servico"];
+    $descricaoServico = addslashes($_POST["descricaoServico"]);
+    date_default_timezone_set("Brazil/East");
+    $dataServico = date('yy,m,d');
+    $status = "pendente";
 
-require_once("../00 - BD/bd_conexao.php");
-$sql1 = "SELECT * FROM arvore WHERE IdArvore = $idArvore";
-$result = $con->query($sql1);
-$info = mysqli_fetch_object($result);
-if (mysqli_num_rows($result) == 0) {
-    echo "<h2>  Nao existe essa árvore, por isso nao se pode fazer um registro. Tente novamente. </h2> ";
-} else {
-    $sql2 = "INSERT INTO servico(descricao, codArvore, tipoServico, dataServico, usuSolicitante, statusSer) VALUES
-    ('$descricaoServico', '$idArvore', '$tipoServico', '$dataServico', '$idUsu', '$status')"; // Por que todos os valores dever ser passados com '' mesmo nao sendo string
-    $resultado = $con->query($sql2);
-
-    echo mysqli_error($con);
-
-    if ($resultado == TRUE) {
-        header("Location:solicitacoes.php?ok");
-        echo "aee deu certo";
+    echo "id arvore" . $idArvore;
+    require_once("../00 - BD/bd_conexao.php");
+    $sql1 = "SELECT * FROM arvore WHERE IdArvore = $idArvore";
+    $result = $con->query($sql1);
+    $info = mysqli_fetch_object($result);
+    if (mysqli_num_rows($result) == 0) {
+        echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=gerenciamento_arvores.php'>
+    <script type=\"text/javascript\">
+        alert(\"Nao existe essa árvore, por isso nao se pode fazer um registro. Tente novamente. \");
+    </script>
+";
     } else {
-        header("Location:solicitacoes.php?not");
-        echo "nao deu certo";
+        $sql2 = "INSERT INTO servico(descricao, codArvore, tipoServico, dataServico, usuSolicitante, statusSer) VALUES
+    ('$descricaoServico', '$idArvore', '$tipoServico', '$dataServico', '$idUsu', '$status')"; // Por que todos os valores dever ser passados com '' mesmo nao sendo string
+        $resultado = $con->query($sql2);
+
+        echo mysqli_error($con);
+        fecharConexao($con);
+        if ($resultado == TRUE) {
+            echo "
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=solicitacoes.php'>
+            <script type=\"text/javascript\">
+                alert(\"Serviço solicitado com sucesso. Veja aqui as suas solicitações:\");
+            </script>
+        ";
+
+            echo "serviço solicitado";
+        } else {
+            echo "
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=gerenciamento_arvores.php'>
+            <script type=\"text/javascript\">
+                alert(\"erro solicitar serviço. Tente novamente\");
+            </script>
+        ";
+
+            echo "erro solicitar serviço";
+        }
     }
+} else {
+    echo "
+    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=gerenciamento_arvores.php'>
+    <script type=\"text/javascript\">
+        alert(\"ID inválido. Tente novamente\");
+    </script>";
 }
-fecharConexao($con);
