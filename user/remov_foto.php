d3219a1f78db31177758a0d860d7c5f0014ca80e
Breno Vambaster <brenovambaster5@gmail.com>
2020-06-12 18:34:54 -0300

    excluir  ultima foto registrada

--- user/remov_foto.php
+++ user/remov_foto.php
@@ -5,8 +5,17 @@ $id = $_SESSION['idUsu'];
 
 if (isset($_GET['remov'])) {
 
+
+    $sql = "SELECT foto from usuario WHERE idUsuario=$id";
+    $result = $con->query($sql);
+    $info = $result->fetch_array(MYSQLI_ASSOC);
+    $foto = $info['foto'];
+    if ($foto != 'perfil.png')
+        unlink("foto_perfil/$foto");
+
     $sql = "UPDATE usuario SET  foto='perfil.png' Where idUsuario='$id'";
     $resultado = $con->query($sql);
+
     if ($con->query($sql) === TRUE) {
         echo "success remover";
         echo "
